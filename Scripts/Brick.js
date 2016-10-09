6656d482cc8901e9f979a0673c5d7d5ae5e38337
Pavel Ilchev <pavel_ilchev@abv.bg>
2016-10-09 21:55:00 +0300

    on load event on image

--- Scripts/Brick.js
+++ Scripts/Brick.js
@@ -2,16 +2,18 @@ class Brick extends GameObject {
     constructor(color, ctx) {
         super();
         this.bricksPositions = {"red": {"row": 0, "col": 0}};
-
-        this.img.src = "resources/sprite.png";
         this.size = 64;
         this.row = this.bricksPositions[color]["row"];  //row from sprite
         this.col = this.bricksPositions[color]["col"];
         this.ctx = ctx;
     }
 
-    draw(){
-        let ballPic = document.getElementById('sprite');
-        this.ctx.drawImage(this.img, 0, 0);
+    draw() {
+        let ctx = this.ctx;
+        let img = this.img;
+        this.img.onload = function() {
+            ctx.drawImage(img, 0, 0);
+        };
+        this.img.src = "resources/sprite.png";
     }
 }
--- Scripts/main.js
+++ Scripts/main.js
@@ -1,12 +1,9 @@
 function main(){
     let canvas = document.getElementById('canvas');
     let ctx = canvas.getContext('2d');
-    let brickImage = new Image();
-    brickImage.src = "resources/sprite.png";
-    ctx.drawImage(brickImage, 0, 0);
 
-   // let brick = new Brick("red", ctx);
-   // brick.draw();
+   let brick = new Brick("red", ctx);
+   brick.draw();
 }
 
 main();
