1373cf933f66f3ece1e071ad5aa8e214d284f343
Pavel Ilchev <pavel_ilchev@abv.bg>
2016-10-10 14:28:42 +0300

    add first level

--- Scripts/Brick.js
+++ Scripts/Brick.js
@@ -1,7 +1,13 @@
 class Brick extends GameObject {
     constructor(color, ctx, x, y) {
         super();
-        this.bricksPositions = {"red": {"row": 0, "col": 0}, "purple": {"row": 0, "col": 1}, "yellow": {"row": 0, "col": 2}};
+        this.bricksPositions = {
+            "red": {"row": 0, "col": 0},
+            "purple": {"row": 0, "col": 1},
+            "yellow": {"row": 0, "col": 2}
+        };
+        
+        this.img.src = "resources/sprite.png";
         this.size = 33;
         this.row = this.bricksPositions[color]["row"];  //row from sprite
         this.col = this.bricksPositions[color]["col"];
@@ -13,9 +19,7 @@ class Brick extends GameObject {
     draw() {
         let _this = this;
         this.img.onload = function () {
-            _this.ctx.drawImage(_this.img,( _this.col * _this.size) + (_this.col * 8), _this.row, _this.size, _this.size, _this.x, _this.y,_this.size, _this.size);
+            _this.ctx.drawImage(_this.img, ( _this.col * _this.size) + (_this.col * 8), _this.row, _this.size, _this.size, _this.x * 68, _this.y * 34, _this.size * 2, _this.size);
         };
-
-        this.img.src = "resources/sprite.png";
     }
 }
--- Scripts/main.js
+++ Scripts/main.js
@@ -2,15 +2,31 @@ function main() {
     let canvas = document.getElementById('canvas');
     let ctx = canvas.getContext('2d');
 
-    let colors = ['red', 'purple', 'yellow'];
-
-    for (let i = 0; i < 640; i += 34) {
-        for (let j = 0; j < 170; j += 34) {
-            let rnd = Math.floor(Math.random() * 3);
-            let brick = new Brick(colors[rnd], ctx, i, j);
-            brick.draw();
-        }
-    }
+    let level = getFirstLevel();
+    drawLevel(level,ctx);
 }
 
 main();
+
+function getFirstLevel() {
+    let level = [
+        ["empty", "empty", "red", "empty", "empty", "empty", "empty", "red", "empty", "empty"],
+        ["empty", "red", "empty", "empty", "purple", "yellow", "empty", "empty", "red", "empty"],
+        ["red", "empty", "empty", "empty", "yellow", "purple", "empty", "empty", "empty", "red"],
+        ["empty", "red", "empty", "empty", "purple", "yellow", "empty", "empty", "red", "empty"],
+        ["empty", "empty", "red", "empty", "empty", "empty", "empty", "red", "empty", "empty"]
+    ];
+
+    return level;
+}
+
+function drawLevel(level,ctx) {
+    for (let i = 0; i < level.length; i++) {
+        for (let j = 0; j < level[i].length; j++) {
+            if (level[i][j] != "empty") {
+                let brick = new Brick(level[i][j], ctx, j, i);
+                brick.draw();
+            }
+        }
+    }
+}
